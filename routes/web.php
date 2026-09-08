b042bfc27cc8f89db7703c7a4ec6d07ec4c83460
cihuwawa <salwahusna247@gmail.com>
2026-09-09 01:37:31 +0700

    [P-2] setup routing resource semua tabel

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,38 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\MemberController;
+use App\Http\Controllers\LoanController;
+
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::resource('books', BookController::class);
+
+
+Route::resource('categories', CategoryController::class)
+    ->except(['show']);
+
+
+Route::resource('members', MemberController::class);
+
+
+Route::resource('loans', LoanController::class);
+
+
+Route::put('/loans/{id}/kembalikan',
+    [LoanController::class, 'kembalikan'])
+    ->name('loans.kembalikan');
+
+Route::prefix('admin')->group(function () {
+
+    Route::get('/info', function () {
+        return 'Halaman Admin Perpustakaan';
+    });
+
 });
